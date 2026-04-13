de3c9d16204422e316734b71e3c004d3fe2e53ce
Timofey Derevyankin <timofey.derevyankin@onlyoffice.com>
2026-04-13 21:01:43 +0300

    add gitignore

--- include/logger.hpp
+++ include/logger.hpp
@@ -31,10 +31,6 @@ private:
         {
             std::cerr << "LOGGER ERROR: file was not opened\n";
         }
-        else
-        {
-            log_file.open("../logs/server.log", std::ios::app);
-        }
     }
     ~Logger()
     {
