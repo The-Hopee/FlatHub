ed8e77ddfd6435d417d84ff1b15912732ba6e28b
Timofey Derevyankin <timofey.derevyankin@onlyoffice.com>
2026-05-27 20:34:47 +0300

    commit old version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tima/FlatHub
+# Build directory: /home/tima/FlatHub/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(UserRepositoryTests "/home/tima/FlatHub/build/tests")
+set_tests_properties(UserRepositoryTests PROPERTIES  _BACKTRACE_TRIPLES "/home/tima/FlatHub/CMakeLists.txt;72;add_test;/home/tima/FlatHub/CMakeLists.txt;0;")
